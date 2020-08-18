fd800360070eb55abf0bc9fdcb1c9a4c21b76986
Shota Takinami <shota_t@i.softbank.jp>
2020-08-18 15:37:47 +0900

    Initial Commit

---
+++ ListInUICollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListInUICollectionView
+//
+//  Created by 滝浪翔太 on 2020/08/18.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
